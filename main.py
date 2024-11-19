99eccab32dd9441800fce68246a46d82bb2eb0f7
alphadekw <163714728+alphadekw@users.noreply.github.com>
2024-11-19 22:28:58 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+#1st program
+from unicodedata import numeric
+
+print(9 ** 0.5 * 5)
+
+#2st program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+#3st program
+
+result = (2 * 2 + 2)
+print(result)
+
+result_two = (2 * (2 + 2))
+print(result_two)
+
+print(result == result_two)
+
+#4st program
+number = '123.456'
+print(number[4])
+print(float(number))
+print(float(123.456 * 10))
+print(float(int (123.456 * 10 % 10)))
+
+
